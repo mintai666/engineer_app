b7f1a71d5f8b2d52363214e8ad52ab7f319a63b7
mintai666 <poly.zharovina@gmail.com>
2026-02-28 17:32:33 +0500

    сервер

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
-# from config import bot, dp, router
-from aiogram import Bot, Dispatcher, Router
+import threading
+from aiogram import Bot, Dispatcher
 from data import init_db
 from handlers import router
 import os
-from config import token
+from config import token, folder
+from server import create_app
+
 
 os.environ["PATH"] += os.pathsep + os.getcwd()
+
+def run_flask():
+    app = create_app(folder)
+    print("--- Flask сервер запускается на http://127.0.0.1:5000 ---")
+    # debug=True может конфликтовать с потоками, для начала ставим False
+    app.run(port=5000, debug=False, use_reloader=False)
 
 async def main():
     dp = Dispatcher()
@@ -13,9 +21,13 @@ async def main():
     dp.startup.register(startup)
     dp.shutdown.register(shutdown)
     dp.include_router(router=router)
-    await dp.start_polling(bot)
     init_db()
+    flask_thread = threading.Thread(target=run_flask)
+    flask_thread.daemon = True # Поток умрет сам при выключении основного кода
+    flask_thread.start()
 
+    await dp.start_polling(bot)
+    
 async def startup():
     print('Включен')
 
